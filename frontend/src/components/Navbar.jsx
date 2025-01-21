04536d5027020bbffff3e1e8c565c8dac2c02ad0
Udara Lakshan <ulakshan061@gmail.com>
2025-01-21 20:59:13 +0530

    Add cartItems display to Navbar for better cart visibility

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { ShoppingCart, SquarePlus } from "lucide-react"
 import { Link } from "react-router-dom"
 
-const Navbar = ({ isAdmin, setIsAddOpen, setIsCartOpen }) => {
+const Navbar = ({ isAdmin, setIsAddOpen, setIsCartOpen, cartItems }) => {
 	return (
 		<div className="max-w-screen-xl mx-auto">
 			<div className="flex h-16 items-center justify-between">
@@ -16,12 +16,17 @@ const Navbar = ({ isAdmin, setIsAddOpen, setIsCartOpen }) => {
 						<SquarePlus />
 					</button>
 				) : (
-					<button
-						className="dark:text-white text-black bg-gray-800 p-2 rounded-md hover:bg-gray-200 dark:hover:bg-gray-700"
-						onClick={() => setIsCartOpen(true)}
-					>
-						<ShoppingCart />
-					</button>
+					<div className="relative">
+						<button
+							className="dark:text-white text-black bg-gray-800 p-2 rounded-md hover:bg-gray-200 dark:hover:bg-gray-700"
+							onClick={() => setIsCartOpen(true)}
+						>
+							<ShoppingCart />
+						</button>
+						<div className="bg-blue-400 w-6 rounded-full flex items-center justify-center text-gray-900 font-semibold absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2">
+							{cartItems.length}
+						</div>
+					</div>
 				)}
 			</div>
 		</div>
