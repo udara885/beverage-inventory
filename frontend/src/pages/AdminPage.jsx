c6d5029e5fb9e59da0485ba7fc1f8867a651ef4a
Udara Lakshan <ulakshan061@gmail.com>
2025-01-28 23:51:08 +0530

    Enhance AdminPage component by adding order detail management props

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,13 +1,23 @@
 import HomePage from "./HomePage"
 
-const AdminPage = ({ isAdmin, setIsAddOpen, setIsUpdateOpen, setIsDetailOpen, setId }) => {
+const AdminPage = ({
+	isAdmin,
+	setIsAddOpen,
+	setIsUpdateOpen,
+	setIsDetailOpen,
+	setId,
+	setOrderId,
+	setIsOrderDetailOpen,
+}) => {
 	return (
 		<HomePage
 			isAdmin={isAdmin}
 			setIsAddOpen={setIsAddOpen}
-      setIsUpdateOpen={ setIsUpdateOpen }
-      setIsDetailOpen={ setIsDetailOpen }
+			setIsUpdateOpen={setIsUpdateOpen}
+			setIsDetailOpen={setIsDetailOpen}
 			setId={setId}
+			setOrderId={setOrderId}
+			setIsOrderDetailOpen={setIsOrderDetailOpen}
 		/>
 	)
 }
