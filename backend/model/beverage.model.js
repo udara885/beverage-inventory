534b71a81b6190ed0de9fb0c71ac5e50c126b305
Udara Lakshan <ulakshan061@gmail.com>
2025-01-30 22:43:51 +0530

    Add category field to beverage model with required validation

--- backend/model/beverage.model.js
+++ backend/model/beverage.model.js
@@ -14,6 +14,10 @@ const beverageSchema = new mongoose.Schema(
 			type: Number,
 			default: 1,
 		},
+		category: {
+			type: String,
+			required: true,
+		},
 		description: {
 			type: String,
 			required: true,
