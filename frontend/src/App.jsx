adf618f4f70b32a7c5f29ba60f7d4bbd57ced007
Udara Lakshan <ulakshan061@gmail.com>
2025-02-06 12:06:53 +0530

    Refactor App component to integrate Socket.io for real-time order updates and optimize props with useMemo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { Route, Routes, useLocation } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import HomePage from "./pages/HomePage"
 import AdminPage from "./pages/AdminPage"
-import { Toaster } from "react-hot-toast"
+import toast, { Toaster } from "react-hot-toast"
 import AddModal from "./components/AddModal"
 import UpdateModal from "./components/UpdateModal"
 import CartModal from "./components/CartModal"
@@ -11,6 +11,7 @@ import DetailModal from "./components/DetailModal"
 import OrderDetailModal from "./components/OrderDetailModal"
 import OrderUpdateModal from "./components/OrderUpdateModal"
 import PaymentModal from "./components/PaymentModal"
+import { io } from "socket.io-client"
 
 function App() {
 	const location = useLocation()
@@ -29,7 +30,11 @@ function App() {
 	const [id, setId] = useState("")
 	const [orderId, setOrderId] = useState("")
 	const [cartItems, setCartItems] = useState([])
-	const [view, setView] = useState("menu")
+	const [ view, setView ] = useState( "menu" )
+	
+	const audio = new Audio( "../winner-bell-game-show-91932.mp3" )
+	
+	const socket = io("http://localhost:3000")
 
 	useEffect(() => {
 		if (location.pathname === "/admin") {
@@ -37,16 +42,57 @@ function App() {
 		}
 	}, [location.pathname])
 
+	useEffect(() => {
+		if (isAdmin) {
+			socket.on("received-order", (message) => {
+				toast.success( message )
+				audio.play()
+			})
+		}
+	}, [socket])
+
+	const memoizedNavbarProps = useMemo(
+		() => ({
+			isAdmin,
+			setIsAddOpen,
+			setIsCartOpen,
+			cartItems,
+			setView,
+		}),
+		[isAdmin, cartItems]
+	)
+
+	const memoizedHomePageProps = useMemo(
+		() => ({
+			cartItems,
+			setCartItems,
+			setIsDetailOpen,
+			setId,
+			view,
+			isAdmin,
+		}),
+		[cartItems, view, isAdmin]
+	)
+
+	const memoizedAdminPageProps = useMemo(
+		() => ({
+			isAdmin,
+			setIsAddOpen,
+			setIsUpdateOpen,
+			setIsDetailOpen,
+			setId,
+			setOrderId,
+			setIsOrderDetailOpen,
+			setIsOrderUpdateOpen,
+			view,
+		}),
+		[isAdmin, view]
+	)
+
 	return (
 		<div className="min-h-screen px-5 py-5 bg-gray-900">
 			<Toaster />
-			<Navbar
-				isAdmin={isAdmin}
-				setIsAddOpen={setIsAddOpen}
-				setIsCartOpen={setIsCartOpen}
-				cartItems={cartItems}
-				setView={setView}
-			/>
+			<Navbar {...memoizedNavbarProps} />
 			{isAddOpen && <AddModal setIsAddOpen={setIsAddOpen} />}
 			{isUpdateOpen && (
 				<UpdateModal
@@ -60,7 +106,7 @@ function App() {
 					cartItems={cartItems}
 					setCartItems={setCartItems}
 					newOrder={newOrder}
-					setNewOrder={ setNewOrder }
+					setNewOrder={setNewOrder}
 					setIsPaymentOpen={setIsPaymentOpen}
 				/>
 			)}
@@ -91,37 +137,17 @@ function App() {
 					newOrder={newOrder}
 					setCartItems={setCartItems}
 					setNewOrder={setNewOrder}
+					socket={socket}
 				/>
 			)}
 			<Routes>
 				<Route
 					path="/"
-					element={
-						<HomePage
-							cartItems={cartItems}
-							setCartItems={setCartItems}
-							setIsDetailOpen={setIsDetailOpen}
-							setId={setId}
-							view={ view }
-							isAdmin={isAdmin}
-						/>
-					}
+					element={<HomePage {...memoizedHomePageProps} />}
 				/>
 				<Route
 					path="/admin"
-					element={
-						<AdminPage
-							isAdmin={isAdmin}
-							setIsAddOpen={setIsAddOpen}
-							setIsUpdateOpen={setIsUpdateOpen}
-							setIsDetailOpen={setIsDetailOpen}
-							setId={setId}
-							setOrderId={setOrderId}
-							setIsOrderDetailOpen={setIsOrderDetailOpen}
-							setIsOrderUpdateOpen={setIsOrderUpdateOpen}
-							view={view}
-						/>
-					}
+					element={<AdminPage {...memoizedAdminPageProps} />}
 				/>
 			</Routes>
 		</div>
