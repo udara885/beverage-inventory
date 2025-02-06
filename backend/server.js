8fad6e6071ba7e79d01079555e4f354a0c29be1d
Udara Lakshan <ulakshan061@gmail.com>
2025-02-06 12:05:58 +0530

    Implement Socket.io for real-time order updates

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,27 @@ import path from "path"
 import { connectDB } from "./config/db.js"
 import beverageRoutes from "./routes/beverage.routes.js"
 import orderRoutes from "./routes/order.routes.js"
+import { createServer } from "http"
+import { Server } from "socket.io"
 
 dotenv.config()
 
 const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+	cors: {
+		origin: ["http://localhost:5173"],
+	},
+})
 const PORT = process.env.PORT || 3000
 
 const __dirname = path.resolve()
+
+io.on("connection", (socket) => {
+	socket.on("place-order", (data) => {
+		socket.broadcast.emit("received-order", data.message)
+	})
+})
 
 app.use(express.json())
 
@@ -23,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
 	})
 }
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
 	connectDB()
 	console.log(`Server is running on http://localhost:${PORT}`)
 })
